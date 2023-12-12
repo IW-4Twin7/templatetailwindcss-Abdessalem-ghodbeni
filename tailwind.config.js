4c2ab441075fde7cc81b745ec0c8df8fda480502
Abdessalem-ghodbeni <slouma4ghodbeny@gmail.com>
2023-12-12 19:11:26 +0100

    makkette avec tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,32 @@ module.exports = {
   content: ["./src/*"],
   theme: {
     extend: {
-     
-
-
-      colors:{
-
-        navColor:"#314584",
-        btnColor:"#ff3946",
-        pColor:"#6e757a"
+      colors: {
+        navColor: "#314584",
+        btnColor: "#ff3946",
+        pColor: "#6e757a",
+        div_custumer_color: "#e64c47",
       },
-      fontSize:{
-
-        navSize:"24px",
-        titleSize:"74px",
-        pSize:"18px"
+      fontSize: {
+        navSize: "24px",
+        texttSize: "28px",
+        SectionTwoSize: "45px",
+        TitleFooter: "50px",
+        titleSize: "74px",
+        pSize: "18px",
+        pSizeP2: "18px",
       },
 
-      fontFamily:{
-
-        fontNav:["Work Sans"]
-      }
+      fontFamily: {
+        fontNav: ["Work Sans"],
+      },
+      width: {
+        custumer_div_width: "304px",
+      },
+      height: {
+        custumer_div_height: "385px",
+      },
     },
   },
   plugins: [],
-}
-
+};
